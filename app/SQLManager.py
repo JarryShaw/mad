6fa880cb22b335c48e4ee0e278a9bb7e8f111e78
zoufutai <369150573@qq.com>
2018-11-30 17:05:17 +0800

    .

---
+++ app/SQLManager.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from peewee import *
+
+db = MySQLDatabase(
+    database='deepocean',
+    host='localhost',
+    port=3306,
+    user='root',
+    passwd='zft13917331612',
+    charset='utf8'
+)
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Mad_Report(BaseModel):
+    name = CharField()
+    is_malicious = BooleanField()
+    ua = CharField()
+    url = CharField()
+    srcip = CharField()
+    srcport = IntegerField()
+    dstip = CharField()
+    dstport = IntegerField()
+    time = DateTimeField()
+    detected_by_cnn = BooleanField()
+    device = CharField()
+    os = CharField()
+    browser = CharField()
+    type = CharField()
+
+
+def saveReport(reportList):
+    for report in reportList:
+        Mad_Report(
+            name=report['name'],
+            is_malicious=report['is_malicious'],
+            ua=report['UA'],
+            url=report['url'],
+            srcip=report['srcIP'],
+            srcport=report['srcPort'],
+            dstip=report['dstIP'],
+            dstport=report['dstPort'],
+            time=report['time'],
+            detected_by_cnn=report['detected_by_cnn'],
+            device=report['info']['device'],
+            os=report['info']['os'],
+            browser=report['info']['browser'],
+            type=report['info']['type']
+        )
