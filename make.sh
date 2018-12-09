169caef8592346b6c9b699a65d6eb10a0f8f586c
Jarry Shaw <JarryShaw@users.noreply.github.com>
2018-12-09 14:08:43 +0800

    Regular update

--- make.sh
+++ make.sh
@@ -26,9 +26,9 @@ cp -rf app/mad.py \
        app/webgraphic apt/app && \
 cp -rf www/* apt/www && \
 sed 's/python_version = "3.6"/python_version = "3.5"/' Pipfile > apt/Pipfile && \
-echo '<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.' > apt/README.md && \
-echo >> apt/README.md && \
-cat README.md >> apt/README.md
+head -2 README.md > apt/README.md && \
+echo '<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.' >> apt/README.md && \
+tail -n +2 README.md >> apt/README.md
 returncode=$?
 if [[ $returncode -ne "0" ]] ; then
     exit $returncode
