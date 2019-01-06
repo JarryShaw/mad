8e1e8306af691845139f5f3b0aef740976fee09c
Jarry Shaw <JarryShaw@users.noreply.github.com>
2019-01-06 21:58:49 +0800

    Regular update  - upper default AS limit

--- app/init.sh
+++ app/init.sh
@@ -11,7 +11,7 @@ python3 run_mad.py --path /mad/pcap \
                    --process=5 \
                    --memlock=2097152 \
                    --vmem=2097152 \
-                   --address-space=2097152 \
+                   --address-space=1073741824 \
                    --swap=2097152
 returncode=$?
 if [[ $returncode -ne "0" ]] ; then
