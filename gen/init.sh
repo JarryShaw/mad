6d40822e6093023513d19d246b1a006ba2acae47
Jarry Shaw <JarryShaw@users.noreply.github.com>
2019-01-09 09:51:07 +0800

    Regular update  - update apt repo  - revised README  - revised init scripts  - updated .dockerignore

--- gen/init.sh
+++ gen/init.sh
@@ -14,7 +14,6 @@ if [[ $returncode -ne "0" ]] ; then
 fi
 
 # run generator
-# python3 generate_report.py | ts "%Y-%m-%dT%H:%M:%.SZ"
 python3 generate_report.py
 returncode=$?
 if [[ $returncode -ne "0" ]] ; then
